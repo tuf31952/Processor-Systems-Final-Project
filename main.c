f404a735c6b4afb64a15612b914c5d516c2aef21
Nikolas Meyer <46723183+tuf31952@users.noreply.github.com>
2019-05-12 23:16:07 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication7.c
+ *
+ * Created: 2/22/2019 10:23:27 AM
+ * Author : Nik
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
